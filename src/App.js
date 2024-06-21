25344e3874c512da5998a5733e3fcd906612cb54
vaishnavi <chourasiavaishnavi01@gmail.com>
2024-06-20 17:16:25 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm.js';
+import TodoList from './components/TodoList.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = (todo) => {
+        setTodos([ ...todos, todo ]);
+    };
+
+    const toggleComplete = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    };
+
+    const removeTodo = (index) => {
+        const newTodos = [...todos];
+        newTodos.splice(index, 1);
+        setTodos(newTodos);
+    };
+
+    return (
+        <div className="App">
+            <h1>Todo List</h1>
+            <TodoForm addTodo={addTodo} />
+            <TodoList todos={todos} toggleComplete={toggleComplete} removeTodo={removeTodo} />
+        </div>
+    );
 }
 
 export default App;
